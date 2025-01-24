3e3eaaea7579ca58ff6666ebb548f0afedd5878e
Joaoapbrito1 <joao.brito@zup.com.br>
2025-01-24 16:03:52 -0300

    refactor: changes Portuguese words to English in the ProductDTO class.

--- src/main/java/com/example/retornosAPI/dto/ProductDTO.java
+++ src/main/java/com/example/retornosAPI/dto/ProductDTO.java
@@ -4,22 +4,22 @@ package com.example.retornosAPI.dto;
 import jakarta.validation.constraints.*;
 
 public record ProductDTO(
-        @NotBlank(message = "O nome do produto é obrigatório.")
-        @Size(min = 3, max = 100, message = "O nome deve ter entre 3 e 100 caracteres.")
+        @NotBlank(message = "Product name is required.")
+        @Size(min = 3, max = 100, message = "The name must be between 3 and 100 characters.")
         String name,
 
-        @Size(max = 500, message = "A descrição pode ter no máximo 500 caracteres.")
+        @Size(max = 500, message = "The description can be a maximum of 500 characters.")
         String description,
 
-        @NotNull(message = "O preço é obrigatório.")
-        @Positive(message = "O preço deve ser maior que 0.")
+        @NotNull(message = "The price is mandatory.")
+        @Positive(message = "Price must be greater than 0.")
         Double price,
 
-        @NotNull(message = "A quantidade em estoque é obrigatória.")
-        @Min(value = 0, message = "A quantidade em estoque deve ser maior ou igual a 0.")
+        @NotNull(message = "Quantity in stock is mandatory.")
+        @Min(value = 0, message = "The quantity in stock must be greater than or equal to 0.")
         Integer stockQuantity,
 
-        @NotBlank(message = "A categoria é obrigatória.")
+        @NotBlank(message = "The category is mandatory.")
         String category){
 
     public ProductDTO(String name, String description, Double price, Integer stockQuantity, String category) {
